ec6bbd2c5411bb4f9e61bf717dab9e063cf6192c
Daniel Bäckström <danbacks@student.chalmers.se>
2013-09-16 01:41:24 +0200

    Added empty projects with some simple logging

---
+++ CopsAndCrooks-android/gen/com/dat255/project/android/copsandcrooks/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package com.dat255.project.android.copsandcrooks;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
