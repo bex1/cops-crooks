cd91e4d38c585727d31a93883a2b1f51c0d5c8ec
Daniel Bäckström <danbacks@student.chalmers.se>
2013-09-16 16:35:16 +0200

    Changed texture packer name

--- CopsAndCrooks/src/com/dat255/project/android/copsandcrooks/screens/SplashScreen.java
+++ CopsAndCrooks/src/com/dat255/project/android/copsandcrooks/screens/SplashScreen.java
@@ -19,8 +19,8 @@ import com.dat255.project.android.copsandcrooks.CopsAndCrooks;
 public class SplashScreen extends AbstractScreen {
     private Image splashImage;
 
-    public SplashScreen(CopsAndCrooks game ) {
-        super( game );
+    public SplashScreen(CopsAndCrooks game) {
+        super(game);
     }
 
     @Override
@@ -33,27 +33,27 @@ public class SplashScreen extends AbstractScreen {
 
         // here we create the splash image actor; its size is set when the
         // resize() method gets called
-        splashImage = new Image( splashDrawable, Scaling.stretch );
-        splashImage.setFillParent( true );
+        splashImage = new Image(splashDrawable, Scaling.stretch);
+        splashImage.setFillParent(true);
 
         // this is needed for the fade-in effect to work correctly; we're just
         // making the image completely transparent
         splashImage.getColor().a = 0f;
 
         // configure the fade-in/out effect on the splash image
-        splashImage.addAction( sequence( fadeIn( 0.75f ), delay( 1.75f ), fadeOut( 0.75f ),
+        splashImage.addAction(sequence(fadeIn(0.75f),delay(1.75f),fadeOut(0.75f),
             new Action() {
                 @Override
                 public boolean act(
                     float delta )
                 {
                     // the last action will move to the next screen
-                    //game.setScreen( new MenuScreen( game ) );
+                    //game.setScreen(new MenuScreen(game));
                     return true;
                 }
             } ) );
 
         // and finally we add the actor to the stage
-        stage.addActor( splashImage );
+        stage.addActor(splashImage);
     }
 }
