c325725e8dbbc553cd39bd58a90b256ffac8d915
Daniel Bäckström <danbacks@student.chalmers.se>
2013-09-17 16:34:33 +0200

    Implemented IMovable, Crook, AbstractPawn

--- CopsAndCrooks/src/com/dat255/project/android/copsandcrooks/domainmodel/Crook.java
+++ CopsAndCrooks/src/com/dat255/project/android/copsandcrooks/domainmodel/Crook.java
@@ -1,9 +1,58 @@
 package com.dat255.project.android.copsandcrooks.domainmodel;
 
+/**
+ * A crook pawn in the game Cops&Crooks.
+ * 
+ * @author Group 25, course DAT255 at Chalmers Uni.
+ *
+ */
 public class Crook extends AbstractPawn {
-
-	public Crook() {
-		// TODO Auto-generated constructor stub
+	private int cash;
+	
+	/**
+	 * Sets the cash for the crook.
+	 * @param money the cash thr crook will hold.
+	 */
+	public void setCash(int money) {
+		cash = money;
 	}
-
+	
+	/**
+	 * Returns the cash on the crook.
+	 * @return the cash on the crook.
+	 */
+	public int getCash() {
+		return cash;
+	}
+	
+	/**
+	 * Increments the cash amount on the crook.
+	 * @param money the money to increment with
+	 */
+	public void incrementCash(int money) {
+		cash += money;
+	}
+	
+	/**
+	 * Decrements the cash amount on the crook.
+	 * 
+	 * @param money the money to decrement with
+	 * @return true if there was enough cash
+	 */
+	public boolean decrementCash(int money) {
+		if (cash < money) {
+			return false;
+		}
+		cash -= money;
+		return true;
+		
+	}
+	
+	/**
+	 * Returns true if the crook is wanted.
+	 * @return true if the crook is wanted.
+	 */
+	public boolean isWanted() {
+		return cash > 0;
+	}
 }
