f079746e9659d285137288aca549e1c0c09aeae6
Daniel Bäckström <danbacks@student.chalmers.se>
2013-09-17 02:00:44 +0200

    Basic main menu written

---
+++ CopsAndCrooks/src/com/dat255/project/android/copsandcrooks/screens/MenuScreen.java
@@ -0,0 +1,61 @@
+package com.dat255.project.android.copsandcrooks.screens;
+
+import com.badlogic.gdx.scenes.scene2d.InputEvent;
+import com.badlogic.gdx.scenes.scene2d.ui.Table;
+import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
+import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
+import com.dat255.project.android.copsandcrooks.CopsAndCrooks;
+
+public class MenuScreen extends AbstractScreen {
+
+	public MenuScreen(CopsAndCrooks game) {
+		super(game);
+	}
+	
+	@Override
+    public void show() {
+        super.show();
+        // retrieve the default table actor
+        Table table = super.getTable();
+        table.add("Cops & crooks!").spaceBottom(50);
+        table.row();
+
+        // register the button "start game"
+        TextButton startGameButton = new TextButton("Start game", getSkin());
+        startGameButton.addListener(new ClickListener() {
+            @Override
+            public void touchUp(InputEvent event, float x, float y, int pointer, int button) {
+                super.touchUp(event, x, y, pointer, button);
+                // TODO click sound
+                // TODO switch to game screen or (in online -> start new or join existing game) 
+            }
+        } );
+        table.add(startGameButton).size(350, 60).uniform().spaceBottom(10);
+        table.row();
+
+        // register the button "options"
+        TextButton optionsButton = new TextButton( "Options", getSkin() );
+        optionsButton.addListener(new ClickListener() {
+            @Override
+            public void touchUp(InputEvent event, float x, float y, int pointer, int button) {
+                super.touchUp(event, x, y, pointer, button);
+                // TODO click sound
+                // TODO switch to options screen
+            }
+        } );
+        table.add(optionsButton).uniform().fill().spaceBottom(10);
+        table.row();
+
+        // register the button "exit"
+        TextButton highScoresButton = new TextButton("Quit", getSkin());
+        highScoresButton.addListener(new ClickListener() {
+            @Override
+            public void touchUp(InputEvent event, float x, float y, int pointer, int button) {
+                super.touchUp(event, x, y, pointer, button);
+                // TODO click sound
+                // TODO kill app
+            }
+        } );
+        table.add(highScoresButton).uniform().fill();
+    }
+}
