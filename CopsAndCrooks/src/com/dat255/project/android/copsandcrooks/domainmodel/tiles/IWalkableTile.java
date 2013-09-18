e1c1db74ce3aaee96144ea1879bb218ee13c6ac4
Victor <vicsand@student.chalmers.se>
2013-09-18 15:45:45 +0200

    Added an abstractclass for tiles along with interfaces

--- CopsAndCrooks/src/com/dat255/project/android/copsandcrooks/domainmodel/tiles/IWalkableTile.java
+++ CopsAndCrooks/src/com/dat255/project/android/copsandcrooks/domainmodel/tiles/IWalkableTile.java
@@ -1,5 +1,34 @@
 package com.dat255.project.android.copsandcrooks.domainmodel.tiles;
 
-public interface IWalkableTile {
+import javax.vecmath.Vector2d;
 
+/**
+ * This class represents a walkable tile.
+ * 
+ * @author Group 25, course DAT255 at Chalmers Uni.
+ *
+ */
+public interface IWalkableTile {
+	
+	/**
+	 * Checks if the tile is occupied by an IMovable object
+	 * @return true if this tile is occupied
+	 */
+	public boolean isOccupied();
+	
+	/**
+	 * Set the occupied-status of this tile
+	 */
+	public void setOccupied(boolean occupied);
+	
+	/**
+	 * Get the position of this tile
+	 * @return the position of this tile
+	 */
+	public Vector2d getPosition();
+	
+	/**
+	 * Set the position of this tile
+	 */
+	public void setPosition(Vector2d position);
 }
