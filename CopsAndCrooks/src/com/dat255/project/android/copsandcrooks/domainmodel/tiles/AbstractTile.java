c4437a246641795e81b654b1e118497663a94481
Victor <vicsand@student.chalmers.se>
2013-09-18 15:45:45 +0200

    Added an abstractclass for tiles along with interfaces

--- CopsAndCrooks/src/com/dat255/project/android/copsandcrooks/domainmodel/tiles/AbstractTile.java
+++ CopsAndCrooks/src/com/dat255/project/android/copsandcrooks/domainmodel/tiles/AbstractTile.java
@@ -1,9 +1,44 @@
 package com.dat255.project.android.copsandcrooks.domainmodel.tiles;
 
-public abstract class AbstractTile {
+import javax.vecmath.Vector2d;
 
-	public AbstractTile() {
-		// TODO Auto-generated constructor stub
+/**
+ * This class represents all tiles with a position 
+ * and stores whether it's occupied or not.
+ * 
+ * @author Group 25, course DAT255 at Chalmers Uni.
+ *
+ */
+public abstract class AbstractTile implements IWalkableTile{
+
+	private boolean occupied;	
+	private Vector2d position;
+	
+	/**
+	 * Construct a new AbstractTile with a position.
+	 * @param position the tile's position
+	 */
+	public AbstractTile(Vector2d position) {
+		this.position = position;
 	}
 
+	@Override
+	public boolean isOccupied(){
+		return this.occupied;
+	}
+
+	@Override
+	public void setOccupied(boolean occupied) {
+		this.occupied = occupied;
+	}
+	
+	@Override
+	public Vector2d getPosition(){
+		return this.position;
+	}
+	
+	@Override
+	public void setPosition(Vector2d position){
+		this.position = position;
+	}
 }
--- CopsAndCrooks/src/com/dat255/project/android/copsandcrooks/domainmodel/tiles/IInteractiveTile.java
+++ CopsAndCrooks/src/com/dat255/project/android/copsandcrooks/domainmodel/tiles/IInteractiveTile.java
@@ -2,7 +2,13 @@ package com.dat255.project.android.copsandcrooks.domainmodel.tiles;
 
 import com.dat255.project.android.copsandcrooks.domainmodel.IMovable;
 
-public interface IInteractiveTile extends IWalkableTile {
+/**
+ * This class represents all tiles that an IMovable object can interact with.
+ * 
+ * @author Group 25, course DAT255 at Chalmers Uni.
+ *
+ */
+public interface IInteractiveTile{
 	
 	/**
 	 * Allows an IMovable and an IInteractiveTile to interact.
