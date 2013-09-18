bde81377d3098f43e14cb1ff0bf18bcd346f862b
Victor <vicsand@student.chalmers.se>
2013-09-18 15:50:04 +0200

    Removed unused/changed classes

--- CopsAndCrooks/src/com/dat255/project/android/copsandcrooks/domainmodel/tiles/WalkableTile.java
+++
@@ -1,9 +0,0 @@
-package com.dat255.project.android.copsandcrooks.domainmodel.tiles;
-
-public class WalkableTile implements IWalkableTile {
-
-	public WalkableTile() {
-		// TODO Auto-generated constructor stub
-	}
-
-}
