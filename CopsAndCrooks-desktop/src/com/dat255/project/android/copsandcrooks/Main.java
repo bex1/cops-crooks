0f377e6f766aa5ba5eda637b4b38bf26520e813f
unknown <danbacks@student.chalmers.se>
2013-09-23 02:38:35 +0200

    13 points for gryffindor, camera movement up in android

--- CopsAndCrooks-desktop/src/com/dat255/project/android/copsandcrooks/Main.java
+++ CopsAndCrooks-desktop/src/com/dat255/project/android/copsandcrooks/Main.java
@@ -2,14 +2,15 @@ package com.dat255.project.android.copsandcrooks;
 
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.dat255.project.android.copsandcrooks.utils.Constants;
 
 public class Main {
 	public static void main(String[] args) {
 		LwjglApplicationConfiguration cfg = new LwjglApplicationConfiguration();
 		cfg.title = "CopsAndCrooks";
 		cfg.useGL20 = true;
-		cfg.width = 800;
-		cfg.height = 480;
+		cfg.width = Constants.GAME_VIEWPORT_WIDTH;
+		cfg.height = Constants.GAME_VIEWPORT_HEIGHT;
 		
 		new LwjglApplication(new CopsAndCrooks(), cfg);
 	}
