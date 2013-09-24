3521b614cf48c419282e7e843a0b95a5e07dc1b8
Daniel Bäckström <danbacks@student.chalmers.se>
2013-09-24 12:09:52 +0200

    Implemented resize

---
+++ CopsAndCrooks/src/com/dat255/project/android/copsandcrooks/actors/PathActor.java
@@ -0,0 +1,16 @@
+package com.dat255.project.android.copsandcrooks.actors;
+
+import java.beans.PropertyChangeEvent;
+import java.beans.PropertyChangeListener;
+
+import com.badlogic.gdx.scenes.scene2d.ui.Image;
+
+public class PathActor extends Image implements PropertyChangeListener {
+
+	@Override
+	public void propertyChange(PropertyChangeEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
