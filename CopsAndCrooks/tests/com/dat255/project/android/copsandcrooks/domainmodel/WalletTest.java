2ebfc3956666d330d6a69dc9cda4d167bf4d7601
andehall <anders.hallgren@live.se>
2013-09-26 15:36:28 +0200

    added tests for wallet and game model, game model test not yey fully implemented

---
+++ CopsAndCrooks/tests/com/dat255/project/android/copsandcrooks/domainmodel/WalletTest.java
@@ -0,0 +1,51 @@
+package com.dat255.project.android.copsandcrooks.domainmodel;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class WalletTest {
+
+	@Test
+	public void SetCashTest() {
+		Wallet wallet = new Wallet();
+		if(wallet.getCash() < 0){
+			fail("Cash should never be less than 0");
+		}
+		
+		assertTrue(wallet.getCash() == 0);
+		
+		wallet.setCash(4000);
+		
+		assertTrue(wallet.getCash() == 4000);
+		
+	}
+	@Test
+	public void IncrementCashTest(){
+		Wallet wallet = new Wallet();
+		if(wallet.getCash() < 0){
+			fail("Cash should never be less than 0");
+		}
+		
+		wallet.setCash(4000);
+		
+		wallet.incrementCash(2000);
+		
+		assertTrue(wallet.getCash() == 6000);
+		
+	}
+	@Test
+	public void DecrementCashTest(){
+		Wallet wallet = new Wallet();
+		if(wallet.getCash() < 0){
+			fail("Cash should never be less than 0");
+		}
+		
+		wallet.setCash(4000);
+		
+		wallet.decrementCash(2000);
+		
+		assertTrue(wallet.getCash() == 2000);
+	}
+
+}
