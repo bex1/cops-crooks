1ee2b7618e9c588df126ec5743b959ef7510d3fb
unknown <danbacks@student.chalmers.se>
2013-09-26 14:14:02 +0200

    Bugfixes, can now click on EndPathTiles always and all paths are now visually removed when clicked.

--- CopsAndCrooks/src/com/dat255/project/android/copsandcrooks/actors/PathActor.java
+++ CopsAndCrooks/src/com/dat255/project/android/copsandcrooks/actors/PathActor.java
@@ -6,8 +6,11 @@ import com.badlogic.gdx.scenes.scene2d.Actor;
 import com.badlogic.gdx.scenes.scene2d.Group;
 import com.badlogic.gdx.scenes.scene2d.InputEvent;
 import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.scenes.scene2d.Touchable;
 import com.badlogic.gdx.scenes.scene2d.ui.Image;
 import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
+import com.badlogic.gdx.utils.Timer;
+import com.badlogic.gdx.utils.Timer.Task;
 import com.dat255.project.android.copsandcrooks.domainmodel.Player;
 import com.dat255.project.android.copsandcrooks.domainmodel.TilePath;
 
@@ -18,6 +21,24 @@ import com.dat255.project.android.copsandcrooks.domainmodel.TilePath;
  */
 public class PathActor extends Group {
 	private final TilePath tilePath;
+
+	private final Player player;
+	private final Timer removeTimer;
+	
+	private final Task removeTask = new Task() {
+		@Override
+		public void run () {
+			Stage stage = getStage();
+			if (stage != null) {
+				for (Actor actor : stage.getActors()) {
+					if (actor instanceof PathActor) {
+						actor.remove();
+					}
+				}
+			}
+			removeTimer.stop();
+		}
+	};
 	
 	/**
 	 * Instantiates a visual PathActor along the specified path.
@@ -29,44 +50,50 @@ public class PathActor extends Group {
 	 */
 	public PathActor(final TilePath path, final List<Image> pathImages, final Image pathEndImage, final Player player) {
 		this.tilePath = path;
-		
-		for (Image img : pathImages) {
-			this.addActor(img);
-		}
-		
-		final PathActor thisActor = this;
-		
-		pathEndImage.addListener(new ClickListener() {
-			
-			@Override
-			public boolean touchDown(InputEvent event, float x, float y,
-					int pointer, int button) {
-				
-				return super.touchDown(event, x, y, pointer, button);
-			}
-
-			@Override
-			public void touchUp(InputEvent event, float x, float y,
-					int pointer, int button) {
-				player.choosePath(tilePath);
-				
-				Stage stage = getStage();
-				
-				// Fins all other PathActors in stage and remove them.
-				if (stage != null) {
-					for (Actor actor : stage.getActors()) {
-						if (actor instanceof PathActor && actor != thisActor) {
-							actor.remove();
-						}
-					}
-				}
-				
-				// Remove ourself from stage.
-				thisActor.remove();
-			
-			}
-		});
+		this.player = player;
+		this.removeTimer = new Timer();
 		
 		this.addActor(pathEndImage);
+		for (Image img : pathImages) {
+			this.addActor(img);
+			img.setTouchable(Touchable.disabled);
+		}
+		
+		pathEndImage.addListener(click);
 	}
+	
+	private ClickListener click = new ClickListener() {
+		
+		@Override
+		public boolean touchDown(InputEvent event, float x, float y,
+				int pointer, int button) {
+			
+			return super.touchDown(event, x, y, pointer, button);
+		}
+
+		@Override
+		public void touchUp(InputEvent event, float x, float y,
+				int pointer, int button) {
+			player.choosePath(tilePath);
+			
+			final Stage stage = getStage();
+			// Finds all other PathActors in stage and remove them.
+			if (stage != null) {
+				for (Actor actor : stage.getActors()) {
+					if (actor instanceof PathActor) {
+						PathActor pathActor = (PathActor)actor;
+						pathActor.clear();
+					}
+				}
+			}
+			
+			// Can't remove the PathActors in stage before the pathActors has cleared all children
+			// We solve this by scheduling a task that will do this in 0.5 seconds.
+			removeTimer.scheduleTask(removeTask, 0.5f);
+			removeTimer.start();
+		}
+	};
+
+	
+	
 }
