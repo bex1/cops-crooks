3bd7e376fb6448b755385e7d98d77e5cdf517665
MarcusLagerstedt <ml_lagerstedt93@hotmail.com>
2013-10-03 11:32:05 +0200

    Fixed problem that where when you got in prison and you could go by tram and that crooks where placed in hideout twice

--- CopsAndCrooks/src/com/dat255/project/android/copsandcrooks/domainmodel/Officer.java
+++ CopsAndCrooks/src/com/dat255/project/android/copsandcrooks/domainmodel/Officer.java
@@ -28,6 +28,7 @@ public class Officer extends AbstractWalkingPawn implements ISelectablePawn {
 				mediator.moveToPoliceStation(crook);
 				mediator.moveToPoliceStation(this);
 				crook.setIsInPoliceStation(true);
+				crook.standingOnTramstop(false);
 				this.setIsInPoliceStation(true);
 				
 				Wallet crookWallet = crook.getWallet();
